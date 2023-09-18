67d1d87e05c96b76cf6650e0d6e51cb13e3a24e5
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-09-17 22:17:28 -0300

    Update prettier and eslint rules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [
